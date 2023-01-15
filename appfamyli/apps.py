d7f8050ce55e76282f8633ca65afa427d839d41b
TarumK <kyabishev72@gmail.com>
2023-01-15 16:42:38 +0300

    Пример отображаения данных из двух моделей, связанных отношением "один ко многим". С использованием java-script на фронте.

---
+++ appfamyli/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppfamyliConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'appfamyli'
---
+++ appfamyli/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from .models import Parent, Child
+
+class MyForm(forms.Form):
+    parents = forms.ModelChoiceField(queryset=Parent.objects.all())
+    children = forms.ModelChoiceField(queryset=Child.objects.none())
+
+    # def __init__(self, *args, **kwargs):
+    #     super(MyForm, self).__init__(*args, **kwargs)
+    #
+    #     self.fields['children'].queryset = Child.objects.none()
+    #
+    # def clean(self):
+    #     cleaned_data = super(MyForm, self).clean()
+    #     parent = cleaned_data.get("parents")
+    #     children = cleaned_data.get("children")
+    #
+    #     if parent and not children:
+    #         self.fields['children'].queryset = Child.objects.filter(parent=parent)
+
+
+
+
+# from django import forms
+# from .models import Parent, Child
+#
+#
+# class MyForm(forms.Form):
+#     parent = forms.ModelChoiceField(queryset=Parent.objects.all())
+#     child = forms.ModelChoiceField(queryset=Child.objects.all())
+#
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['child'].queryset = Child.objects.none()
+#
+#         if 'parent' in self.data:
+#             try:
+#                 parent_id = int(self.data.get('parent'))
+#                 self.fields['child'].queryset = Child.objects.filter(parent_id=parent_id).order_by('name')
+#             except (ValueError, TypeError):
+#                 pass  # invalid input from the client; ignore and fallback to empty child queryset
+#         elif self.instance.pk:
+#             self.fields['child'].queryset = self.instance.parent.child_set.order_by('name')
+#
+#
